d8c80173a738b66380d2ade90beed18b5fbe69b2
James Vance <jvance@ictp.it>
2017-05-01 22:31:53 +0200

    Update

--- src/uly_md.sh
+++ src/uly_md.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -l walltime=0:59:00
-# PBS -l nodes=1:ppn=20
+#PBS -l walltime=0:10:00
 #PBS -l nodes=1:ppn=20
+# PBS -l nodes=3:ppn=20
 #PBS -q reserved3
 #PBS -T flush_cache
 
@@ -19,14 +19,27 @@ mkdir $results
 timings=$results/timings_$PBS_JOBID.txt
 out=$results/out_$PBS_JOBID.txt
 
-lattice=12
 
 echo "Timings: $timings"
 echo "Lattice: $lattice"
 
 cd $results;
-../../input/lattice $lattice > crystal.xyz 
+
+# ../../input/gen_input_noexchange.py
 ../../input/gen_input.py
+
+lattice=15
+../../input/lattice $lattice > crystal.xyz 
+
+for nprocs in $(seq 16 16)
+do
+    telapsed=$(( (/usr/bin/time -p -f%e mpirun -np $nprocs ../simplemd.x *.params) 1>>$out)  2>&1)
+    echo $lattice $nprocs $telapsed >> $timings
+    $out
+done 
+
+lattice=18
+../../input/lattice $lattice > crystal.xyz 
 
 for nprocs in $(seq 16 16)
 do
