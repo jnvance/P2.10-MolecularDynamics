392145ce82494d2cfbd59fe60d69b955219aa2ba
James Vance <jvance@ictp.it>
2017-05-01 15:18:18 +0200

    Wrote script for running on cluster

---
+++ src/uly_md.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#PBS -l walltime=0:59:00
+# PBS -l nodes=1:ppn=20
+#PBS -l nodes=1:ppn=20
+#PBS -q reserved3
+#PBS -T flush_cache
+
+## For Ulysses
+module load testing; 
+module load openmpi/2.0.0/gnu/6.2.0
+
+cd $PBS_O_WORKDIR
+make clean
+make
+
+results=$PBS_O_WORKDIR/data_$PBS_JOBID
+mkdir $results
+
+timings=$results/timings_$PBS_JOBID.txt
+out=$results/out_$PBS_JOBID.txt
+
+lattice=12
+
+echo "Timings: $timings"
+echo "Lattice: $lattice"
+
+cd $results;
+../../input/lattice $lattice > crystal.xyz 
+../../input/gen_input.py
+
+for nprocs in $(seq 16 16)
+do
+    telapsed=$(( (/usr/bin/time -p -f%e mpirun -np $nprocs ../simplemd.x *.params) 1>>$out)  2>&1)
+    echo $lattice $nprocs $telapsed >> $timings
+    $out
+done 
+
